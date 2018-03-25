16951897d658a80f68b816b6dca5e99c03aa3a13
Petr <petr.sukhov2015@yandex.ru>
2018-03-25 13:35:44 +0300

    add some realization for stub and test class

--- src/test/java/stub/test/TheStubTest/Stub.java
+++ src/test/java/stub/test/TheStubTest/Stub.java
@@ -1,6 +1,35 @@
 package stub.test.TheStubTest;
 
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+
 public class Stub {
+
+	public String getTask(JSONObject jsonId)  {
+
+		//create simple json
+		JSONObject jsonResp = new JSONObject();
+		//int theId = Integer.parseInt((String) jsonId.get("Id") );
+		
+		//case valid id
+		if (theId < 1000){ //valid id
+			jsonResp.put("id", 76699046);
+			jsonResp.put("name", "Task 1");
+			jsonResp.put("mark", 3.5);
+			
+		} else if (1000<theId){ //error id
+			jsonResp.put("code", 404);
+			jsonResp.put("description", "Not Found");
+			
+		} else if (1000 == theId){//no params
+			jsonResp.put("code", 400);
+			jsonResp.put("description", "Bad Request");
+			
+		}
+		return "HTTP/1.1 200 OK\r\n"+jsonResp.toString();
+		
+	}
 	
 
 }
--- src/test/java/stub/test/TheStubTest/StubTest.java
+++ src/test/java/stub/test/TheStubTest/StubTest.java
@@ -1,5 +1,55 @@
 package stub.test.TheStubTest;
 
-public class StubTest {
+import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.JSONPointer;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class StubTest {
+	
+	JSONObject jsonIdObject;
+	Stub s;
+	
+	@BeforeTest
+	public void b() throws ParseException, FileNotFoundException, IOException {
+		//create stub
+		 s = new Stub();
+		
+		//create taskId
+		//String filePath = "D:\\EclipseMars2\\TheStubTest\\json\\taskId.json";
+		//JSONParser jsonParser = new JSONParser();
+		//jsonIdObject = (JSONObject) jsonParser.parse(filePath);
+		
+		JSONParser parser = new JSONParser();
+
+            Object object = parser
+                    .parse(new FileReader("D:\\EclipseMars2\\TheStubTest\\json\\taskId.json"));
+            
+            //convert Object to JSONObject
+            jsonIdObject = (JSONObject)object;
+        
+	}
+	
+
+	@Test
+	public void tValid() throws IOException{
+		//Assert.assertTrue(s.getTask(jsonIdObject), )
+		System.out.println(s.getTask(jsonIdObject));
+	}
+	/*
+	public void tUnValid() throws IOException{
+		System.out.println(s.getTask("123"));
+	}
+	public void tBadParams() throws IOException{
+		
+		System.out.println(s.getTask("123"));
+	}*/
 }
